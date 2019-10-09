872ee96ef2d58e211ca43acc53f0bf5ebc631905
Harry Zhang <harryzhang100@gmail.com>
2019-10-08 23:46:51 -0700

    setting up folder structure of the project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Header extends React.Component {
+  
+  constructor(props) {
+  	super(props);
+  	this.state = {
+
+  	}
+  }	
+
+  render() {
+  	return (
+  	  <div>
+  	  	<p>Sign In</p>
+  	  	<p>Sign Out</p>	
+  	  </div>
+  	)
+  }
+}
+
+
+export const Header;
